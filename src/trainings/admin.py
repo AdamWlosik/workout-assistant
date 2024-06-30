8b3daed38917227d197bc0da2ccab44612fe7d92
Adam Włosik <wlosikk.adam@gmail.com>
2024-06-30 16:49:17 +0200

    Add feature

--- src/trainings/admin.py
+++ src/trainings/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from trainings.models import Category
+from trainings.models import Category, Training, TrainingExercise
+
+
+class ExerciseInline(admin.TabularInline):
+    model = Training.exercises.through
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name",)
+
+
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    inlines = [
+        ExerciseInline,
+    ]
+
+
+@admin.register(TrainingExercise)
+class TrainingExerciseAdmin(admin.ModelAdmin):
+    pass
