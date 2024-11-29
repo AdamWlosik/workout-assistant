c86a0a472085453468c4b975fd36a3ac90fc4b54
Adam <wlosikk.adam@gmail.com>
2024-11-29 19:06:17 +0100

    Add work in progess

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,7 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+
+from calendary.models import Event
 
 
 class HomePageView(TemplateView):
     """Class to display home page view"""
 
-    template_name = "home.html"
+    template_name = "dashboard/home.html"
+
+    @login_required
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["training_amount"] = Event.objects.filter(user=self.request.user).count()
+        context["exercise_amount"] = "TODO"  # to nie potrzebne
+        context["exercises"] = "TODO"
+        return context
+
