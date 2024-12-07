4ef83cf6b53cfd463c80f216994f3592ae16eee3
Adam <wlosikk.adam@gmail.com>
2024-12-07 20:23:49 +0100

    Add reps in calendary view

--- src/trainings/hx_views.py
+++ src/trainings/hx_views.py
@@ -72,7 +72,6 @@ def hx_training_exercise_rep_edit(request: "HttpRequest", relation_id: int, trai
         training_exercise.save()
         return HttpResponse(rep_edit)
 
-
 @login_required
 def hx_training_exercise_rep_add(request: "HttpRequest", relation_id: int, training_id: int) -> "HttpResponse":
     training_exercise = get_object_or_404(TrainingExercise, id=relation_id, training_id=training_id)
@@ -82,12 +81,19 @@ def hx_training_exercise_rep_add(request: "HttpRequest", relation_id: int, train
         return render(request, "trainings/hx_training_exercise_rep_add.html",
                       {"relation_id": relation_id, "training_id": training_id})
     elif request.method == "POST":
-        # Dodaj nowe powtórzenie
         new_rep = request.POST.get("rep_value")
         if new_rep:
-            training_exercise.reps.append(new_rep)  # Dodaj nowe powtórzenie
+            # Dodaj nowe powtórzenie do listy
+            training_exercise.reps.append(new_rep)
             training_exercise.save()
-        return HttpResponse(new_rep)
+            # Zwróć nowy element HTML jako odpowiedź
+            return HttpResponse(
+                f'<div class="btn btn-outline-light" '
+                f'hx-get="{request.build_absolute_uri(request.path)}?edit={len(training_exercise.reps)-1}">'
+                f'{new_rep}</div>'
+            )
+        return HttpResponse(status=400)  # Brak wartości w POST
+
 
 
 # TODO dokończ widok edycji treningu całego
